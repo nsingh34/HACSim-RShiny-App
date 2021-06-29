9fda518b6aa88d31273c48e54e786552c4a54886
nsingh34 <navuonweb@gmail.com>
2021-06-29 06:47:12 -0400

    Commit All

---
+++ server.R
@@ -0,0 +1,15 @@
+# This file defines the back-end of the application
+# server-side functionality
+
+
+
+# importing shiny library
+library(shiny)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  observeEvent(input$run, {
+    
+  })
+}
